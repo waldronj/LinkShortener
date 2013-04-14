65e130842655508e73ee59a7adea37b82668058f
Josh Waldron <jwaldron@thejwal.com>
2013-04-13 22:36:49 -0500

    Look and feel established
    HTML markup completed,
    Bootstrap added
    Views created for Index and Create (no form yet)

--- LinkShortener/App_Start/RouteConfig.cs
+++ LinkShortener/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace LinkShortener
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "URL", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
