e7fd3cf087552f95c4ea520f1cf22749e384fc87
Josh Waldron <jwaldron@thejwal.com>
2013-04-13 23:32:17 -0500

    Should have .gitignore working now

---
+++ LinkShortener/Controllers/URLController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LinkShortener.Controllers
+{
+    public class URLController : Controller
+    {
+        //
+        // GET: /URL/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create()
+        {
+            //DO STUFF
+            return View("Index");
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+LinkShortener
+=============
+After doing a quick search on github, I didn't see a C# .net link shortener repo. So I'm starting from scratch.
